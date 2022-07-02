bfbe13b396bf7564379fb5fb7c75948cf1ff0394
YayoReynoso <eduardo_3reynoso@hotmail.com>
2022-07-02 12:01:25 -0700

    Class code

--- register.js
+++ register.js
@@ -8,68 +8,43 @@ let petSaloon = {
         open:"9:00 am",
         close:"6:00 pm"
     },
-    pets:[
-        { //anonymous object
-            name:"Scooby",
-            age:50,
-            breed:"Dane",
-            gender:"Male",
-            service:"Grooming",
-            ownerName:"Shaggy",
-            contactPhone:"666-666-6666"
-        },
+    pets:[]
 
-        { //anonymous object
-            name:"Dante",
-            age:38,
-            breed:"Mixed",
-            gender:"Male",
-            service:"Nails cut",
-            ownerName:"Eduardo",
-            contactPhone:"666-666-6666"
-        },
-
-        { //anonymous object
-            name:"Rango",
-            age:12,
-            breed:"Mixed",
-            gender:"Male",
-            service:"Vaccine",
-            ownerName:"Mario",
-            contactPhone:"666-666-6666"
-        },
-
-        { //anonymous object
-            name:"Barbie",
-            age:12,
-            breed:"Mixed",
-            gender:"Female",
-            service:"Vaccine",
-            ownerName:"Mary",
-            contactPhone:"666-666-6666"
-        },
-
-        { //anonymous object
-            name:"Speedy Gonzales",
-            age:99,
-            breed:"Mixed",
-            gender:"Male",
-            service:"Vaccine",
-            ownerName:"Pedro",
-            contactPhone:"666-666-6666"
-        }
-
-    ]
+    // //{ //anonymous object
+        //     name:"Scooby",
+        //     age:50,
+        //     breed:"Dane",
+        //     gender:"Male",
+        //     service:"Grooming",
+        //     ownerName:"Shaggy",
+        //     contactPhone:"666-666-6666"
+        // }
+    
 }
+
+//constructor function
+function Pet(name,age,breed,gender,service,ownerName,contactPhone){
+    this.name=name;
+    this.age=age;
+    this.breed=breed;
+    this.gender=gender;
+    this.service=service;
+    this.ownerName=ownerName;
+    this.contactPhone=contactPhone;
+}
+
+
 
 
 function displayNames(){
 
+
+    
     for (let i = 0; i < petSaloon.pets.length; i++) {
         document.getElementById("listPets").innerHTML+=`<li>${petSaloon.pets[i].name}</li>`;
-        
-        
-    }
+            
+            
+    }  
     
 }
 
@@ -79,9 +54,62 @@ function displayInfo(){
 
 
 function displayNPets(){
-    alert(`There's ${petSaloon.pets.length} pets registered.`)
+    document.getElementById("container-pets").innerHTML=(`<p>There's ${petSaloon.pets.length} pets registered.</p>`)
 }
 
-displayNames();
-displayInfo();
-displayNPets();
+
+
+
+
+
+function init(){
+
+    //creating objects
+    let scooby = new Pet("Scooby",20,"Dane","Male","Grooming","Shaggy","666-666-6666");
+
+    let speedyGonzales = new Pet("Speedy",10,"Dane","Male","Grooming","Eduardo","666-666-6666");
+
+    let jessie= new Pet("Jessie",10,"Mixed","Female","Grooming","Pedro","666-666-6666");
+
+    petSaloon.pets.push(scooby,speedyGonzales,jessie);
+    //main function
+    //hook events
+    
+    
+    
+    displayNames();
+    displayInfo();
+
+    
+    
+}
+
+function register(){
+    
+    let petName=document.getElementById("txtPetName").value;
+
+
+    
+    let petAge = document.getElementById("txtPetAge").value;
+
+    let petBreed = document.getElementById("txtPetBreed").value;
+
+    let petGender = document.getElementById("txtPetGender").value;
+
+    let petService = document.getElementById("txtPetService").value;
+
+    let petOwner = document.getElementById("txtPetOwner").value;
+
+    let petOwnerNumber = document.getElementById("txtPetOwnerNumber").value;
+    
+
+    let newPet = new Pet(petName, petAge, petBreed, petGender, petService, petOwner, petOwnerNumber);
+
+    petSaloon.pets.push(newPet);
+
+    displayNPets();
+
+    console.log(petSaloon.pets)
+
+}
+window.onload=init;
